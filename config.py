84fcfb9d8f72486783c2fabc6cbea54ccefd5b16
Kirill "Zkir <mekillot@yandex.ru>
2020-07-23 20:58:26 +0300

    upgrade for blender 2.83

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-blenderExe="d:/tools/blender-2.81a-windows64/blender.exe"
+blenderExe="d:/tools/blender-2.83.3-windows64/blender.exe"
 
 tests =["Church-vozdvizhenskoe", "half-dome", "moscow", "lavra-sp"]
